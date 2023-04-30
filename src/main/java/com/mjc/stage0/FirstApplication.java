0e4b260269035332f01364b764d8d7bc103c699b
MakhmudjonDev <104415570+MakhmudjonDev@users.noreply.github.com>
2023-04-30 11:03:05 +0500

    first commit

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -3,5 +3,6 @@ package com.mjc.stage0;
 public class FirstApplication {
     public static void main(String[] args) {
         // Write your code here!
+        System.out.println("This is my very first step to ace the training!");
     }
 }
