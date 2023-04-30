e591b9eaf59b334099ad79b1d37dace84fc4824a
MakhmudjonDev <104415570+MakhmudjonDev@users.noreply.github.com>
2023-04-30 11:23:41 +0500

    first commit

--- src/main/java/com/mjc/stage0/UpdatedApp.java
+++ src/main/java/com/mjc/stage0/UpdatedApp.java
@@ -3,6 +3,6 @@ package com.mjc.stage0;
 public class UpdatedApp {
 
     public static void main(String[] args) {
-        System.out.println("I'm updated");
+        System.out.println("    I'm updated");
     }
 }
