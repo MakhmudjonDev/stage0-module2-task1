ae702f5c45c062d95b514f6556faa90decd4f47c
MakhmudjonDev <104415570+MakhmudjonDev@users.noreply.github.com>
2023-04-30 11:16:38 +0500

    first commit

---
+++ src/main/java/com/mjc/stage0/PrintWordsApp.java
@@ -0,0 +1,9 @@
+package com.mjc.stage0;
+
+public class PrintWordsApp {
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("is");
+        System.out.println("smth");
+    }
+}
---
+++ src/main/java/com/mjc/stage0/UpdatedApp.java
@@ -0,0 +1,8 @@
+package com.mjc.stage0;
+
+public class UpdatedApp {
+
+    public static void main(String[] args) {
+        System.out.println("I'am updated");
+    }
+}
